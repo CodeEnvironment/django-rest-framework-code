7e208c89b716e0c15e635557156cfea7fcc9e29f
CodeEnvironment <66573061+CodeEnvironment@users.noreply.github.com>
2020-09-16 00:14:04 +0200

    Update admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
+
 from django.contrib import admin
 from .models import Posts, PostsRates
 
 admin.site.register(Posts)
 admin.site.register(PostsRates)
-=======
-from django.contrib import admin
-from .models import Posts, PostsRates
 
-admin.site.register(Posts)
-admin.site.register(PostsRates)
->>>>>>> 8177ab3bb7908bad888b35267b83d5f301b952e9
