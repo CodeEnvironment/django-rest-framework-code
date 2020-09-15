d72af7240b2d7ac3832ff71f6bdafb8ca50502ec
CodeEnvironment <66573061+CodeEnvironment@users.noreply.github.com>
2020-09-16 00:15:58 +0200

    Update models.py

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.db import models
 
 
@@ -15,21 +14,4 @@ class Posts(models.Model):
 
     def __str__(self):
         return self.post_title
-=======
-from django.db import models
 
-
-class PostsRates(models.Model):
-    likes = models.BigIntegerField(default=0)
-    dislikes = models.BigIntegerField(default=0)
-
-
-class Posts(models.Model):
-    post_title = models.CharField(max_length=200)
-    post_body = models.TextField(max_length=1000)
-    rates = models.OneToOneField(
-        PostsRates, on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return self.post_title
->>>>>>> 8177ab3bb7908bad888b35267b83d5f301b952e9
